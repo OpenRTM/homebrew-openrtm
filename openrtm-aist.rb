9da9610317a0c5bb760e4f56744fe877ea032d7b
n-ando <Noriaki.Ando@gmail.com>
2020-10-01 20:49:31 +0900

    Formula for openrtm-aist updated.

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -9,7 +9,7 @@ class OpenrtmAist < Formula
   depends_on "boost"
 
   bottle do
-    root_url "https://github.com/OpenRTM/OpenRTM-aist/releases/download/v1.2.2/"
+    root_url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2/"
     cellar :any
     sha256 "1cd49e108c9b5df15779f3f9283bca5ea394c2d57e87a35562b5b03b557e8818" => :catalina
   end
@@ -28,19 +28,9 @@ class OpenrtmAist < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    # system "cmake", ".", *std_cmake_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test OpenRTM-aist`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "rtm-config --help"
   end
 end
