85222a5ffc9ec7d66cf55c1e585523664f80bf6d
n-ando <Noriaki.Ando@gmail.com>
2020-10-27 15:44:40 +0900

    A cask formula for OpenRTP added.

---
+++ Casks/openrtp.rb
@@ -0,0 +1,11 @@
+cask "openrtp" do
+  version "1.2.2"
+  sha256 "2d67b131dfcbb1a510e840bdb4641d576789c4f0a1ab22a351fd9ab45bec9352"
+  
+  url "https://github.com/OpenRTM/OpenRTP-aist/releases/download/v1.2.2/openrtp_v1.2.2.dmg"
+  name "OpenRTP-aist"
+  desc "Open RT Platform tool chain"
+  homepage "https://openrtm.org/"
+
+  app "OpenRTP.app"
+end
