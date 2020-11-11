49676145c1e9db4f946715d61b35e203c912aa0f
n-ando <Noriaki.Ando@gmail.com>
2020-11-11 23:42:48 +0900

    dmg sum updated.

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -6,7 +6,7 @@
 #============================================================
 cask "openrtp" do
   version "1.2.2"
-  sha256 "1c05f1dcb337b99b089203a7e3b907a65fcb48cf2a4cd676d0db4b9f31f146fb"
+  sha256 "b29d93c333f02322bb919be7eb77355644860dd6bb7ed4929d32b3e3a77253a9"
   
   url "https://github.com/OpenRTM/OpenRTP-aist/releases/download/v1.2.2/openrtp_v1.2.2.dmg"
   name "OpenRTP-aist"
