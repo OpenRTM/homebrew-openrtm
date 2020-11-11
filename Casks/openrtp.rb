31612f2a380ffb15cb30c0eca86aa49e3ed05619
n-ando <Noriaki.Ando@gmail.com>
2020-11-11 22:57:38 +0900

    OpenRTP.app eclipse.ini updated for multiple versions of java

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -6,12 +6,18 @@
 #============================================================
 cask "openrtp" do
   version "1.2.2"
-  sha256 "2d67b131dfcbb1a510e840bdb4641d576789c4f0a1ab22a351fd9ab45bec9352"
+  sha256 "1c05f1dcb337b99b089203a7e3b907a65fcb48cf2a4cd676d0db4b9f31f146fb"
   
   url "https://github.com/OpenRTM/OpenRTP-aist/releases/download/v1.2.2/openrtp_v1.2.2.dmg"
   name "OpenRTP-aist"
   desc "Open RT Platform tool chain"
   homepage "https://openrtm.org/"
 
+  depends_on cask: "homebrew/cask-versions/adoptopenjdk8"
+
   app "OpenRTP.app"
+
+  caveats do
+    depends_on_java "8"
+  end
 end
