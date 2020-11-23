61c44e67f45c89d4770291c6438ae64c65d1cc2a
n-ando <Noriaki.Ando@gmail.com>
2020-11-23 11:16:15 +0900

    Formulae for OpenRTM-aist, OpenRTM-aist-Python on python38 added.

---
+++ openrtm-aist-py38.rb
@@ -0,0 +1,49 @@
+#============================================================
+# OpenRTM-aist formula for HomeBrew
+#
+# Author: Noriaki Ando <Noriaki.Ando@gmail.com>
+# GitHub: https://github.com/OpenRTM/homebrew-openrtm
+#
+# This is the formula for omniORBpy on python3.8 (not 3.9).
+# To use this formula/bottle, switch python 3.9 to python 3.8.
+# $ brew unlink python3 (unlink python 3.9)
+# $ brew link python@3.8
+#============================================================
+class OpenrtmAistPy38 < Formula
+  desc "OpenRTM-aist: RT-Middleware and OMG RTC implementation in C++ implemented by AIST"
+  homepage "https://openrtm.org"
+  url "https://github.com/OpenRTM/OpenRTM-aist/releases/download/v1.2.2/OpenRTM-aist-1.2.2.tar.gz"
+  sha256 "4e25b0e38d9b6690b94e1163285ea8b49aef6b245a2c454586915d425e5830a0"
+  license "LGPL-2.1"
+
+  depends_on "openrtm/omniorb/omniorb-ssl-py38"
+  depends_on "boost"
+
+  bottle do
+    root_url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2/"
+    cellar :any
+    sha256 "1bf6d956dd58787a282201e841428041e8467ac2fdc4cf1fdced72a57523dc74" => :catalina
+  end
+
+  def install
+    args = %W[
+      OPENSSL_CFLAGS=-I/usr/local/opt/openssl/include
+      OEPNSSL_LIBS=-L/usr/local/opt/openssl/lib
+      CFLAGS=-I/usr/local/opt/python@3.8/include
+      LDFLAGS=-L/usr/local/opt/python@3.8/lib
+      CC=gcc-4.9
+      CXX=g++-4.9
+    ]
+    system "./configure", "--without-doxygen",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "-j", "4"
+    system "make", "install"
+  end
+
+  test do
+    system "rtm-config --help"
+  end
+end
---
+++ openrtm-aist-python-py38.rb
@@ -0,0 +1,37 @@
+#============================================================
+# OpenRTM-aist-Python formula for HomeBrew
+#
+# Author: Noriaki Ando <Noriaki.Ando@gmail.com>
+# GitHub: https://github.com/OpenRTM/homebrew-openrtm
+#
+# This is the formula for omniORBpy on python3.8 (not 3.9).
+# To use this formula/bottle, switch python 3.9 to python 3.8.
+# $ brew unlink python3 (unlink python 3.9)
+# $ brew link python@3.8
+#============================================================
+class OpenrtmAistPythonPy38 < Formula
+  desc "OpenRTM-aist: RT-Middleware and OMG RTC implementation in Python implemented by AIST"
+  homepage "https://openrtm.org"
+  url "https://github.com/OpenRTM/OpenRTM-aist-Python/releases/download/v1.2.2/OpenRTM-aist-Python-1.2.2.tar.gz"
+  sha256 "88366dd5d9fefa19ba25cbf42b6d39170ffb65a1fe4c8f48222a13ddbcecfae8"
+  license "LGPL-2.1"
+
+  depends_on "openrtm/omniorb/omniorbpy-py38"
+
+  bottle do
+    root_url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2/"
+    cellar :any
+    sha256 "8cf12e35e8c9d7e874a5b99f5fe060e9c9ce4195b2f9d009adfb96a71908dde0" => :catalina
+  end
+
+  def install
+    comp_dir = "#{prefix}/share/openrtm-1.2/components/python3/" 
+    system "/usr/local/opt/python@3.8/bin/python3.8", "setup.py", "build"
+    system "/usr/local/opt/python@3.8/bin/python3.8", "setup.py", "install", "--prefix=#{prefix}"
+    FileUtils.chmod_R(0755, "#{comp_dir}")
+  end
+
+  test do
+    system "rtcprof_python3 --help"
+  end
+end
