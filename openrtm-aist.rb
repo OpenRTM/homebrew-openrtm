145c546fe1dd2e1e362f7a476d88a76816cdaece
n-ando <Noriaki.Ando@gmail.com>
2021-07-17 12:00:00 +0900

    .

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -7,12 +7,19 @@
 class OpenrtmAist < Formula
   desc "OpenRTM-aist meta packge"
   homepage "https://openrtm.org"
-  url "https://github.com/OpenRTM/OpenRTM-aist/releases/download/v1.2.2/OpenRTM-aist-1.2.2.tar.gz"
-  sha256 "4e25b0e38d9b6690b94e1163285ea8b49aef6b245a2c454586915d425e5830a0"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "LGPL-2.1"
 
   depends_on "openrtm-aist-py39"
 
+  def install
+    ("/dev/null").write <<-EOS.undent
+        #!/bin/sh
+        echo Hello
+    EOS
+  end
+  
   test do
     system "rtm-config", "--help"
   end
