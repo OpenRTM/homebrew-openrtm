8be3b4d3286baaa04e6c75e9ad4fe3f2ec808ccc
n-ando <Noriaki.Ando@gmail.com>
2021-07-21 12:00:00 +0900

    .

--- openrtm-aist.rb
+++ openrtm-aist.rb
@@ -5,21 +5,43 @@
 # GitHub: https://github.com/OpenRTM/homebrew-openrtm
 #============================================================
 class OpenrtmAist < Formula
-  desc "OpenRTM-aist meta packge"
+  desc "OpenRTM-aist: RT-Middleware and OMG RTC implementation in C++ implemented by AIST"
   homepage "https://openrtm.org"
   url "https://github.com/OpenRTM/OpenRTM-aist/releases/download/v1.2.2/OpenRTM-aist-1.2.2.tar.gz"
   sha256 "4e25b0e38d9b6690b94e1163285ea8b49aef6b245a2c454586915d425e5830a0"
-#  url "file:///dev/null"
-#  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "LGPL-2.1"
 
-  depends_on "openrtm-aist-py39"
+  bottle do
+    root_url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2"
+    rebuild 2
+    sha256 cellar: :any, catalina: "040affa6c93010bcdfa7308936bd0be7dea0fcac5074fa350d23c6a16199be40"
+  end
+
+  depends_on "boost"
+
+  depends_on "openrtm/omniorb/omniorb-ssl-py39"
+
+  patch do
+    url "https://raw.githubusercontent.com/OpenRTM/homebrew-openrtm/master/Patches/rtm-naming.diff"
+    sha256 "a41cbb5d166728ac666860e6354f7269b92c04b58c4235141080b2020be3aaca"
+  end
 
   def install
-    ("/dev/null").write <<-EOS
-        #!/bin/sh
-        echo Hello
-    EOS
+    args = %w[
+      OPENSSL_CFLAGS=-I/usr/local/opt/openssl/include
+      OEPNSSL_LIBS=-L/usr/local/opt/openssl/lib
+      CFLAGS=-I/usr/local/opt/python@3.9/include
+      LDFLAGS=-L/usr/local/opt/python@3.9/lib
+      CC=gcc-4.9
+      CXX=g++-4.9
+    ]
+    system "./configure", "--without-doxygen",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "-j", "8"
+    system "make", "install"
   end
 
   test do
