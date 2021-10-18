e20869d6cd9b74faa93b86f664f8151c37150114
n-ando <Noriaki.Ando@gmail.com>
2021-10-18 12:00:00 +0900

    .

--- Casks/openrtp.rb
+++ Casks/openrtp.rb
@@ -6,9 +6,9 @@
 #============================================================
 cask "openrtp" do
   version "1.2.2"
-  sha256 "d3596c0d3f3965360d74f6852f5db17b47d3614f837747f54c77a7b5b4099cf2"
+  sha256 "e7a590a0615e81c9721d8acd828e0d9e29ac25ff012e51e2356ffb3e80389897"
   
-  url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2/openrtp_v1.2.2.dmg"
+  url "https://github.com/OpenRTM/homebrew-openrtm/releases/download/1.2.2/openrtp_v1.2.2-2.dmg"
   name "OpenRTP-aist"
   desc "Open RT Platform tool chain"
   homepage "https://openrtm.org/"
